66fc8201491b869f64c88164695bafbe91854946
Michael Keith <mkeith@pulsarastronomy.net>
2021-10-13 11:20:05 +0100

    Added some handy flags to the tempo2 command

--- 3_run_ryans_script.sh
+++ 3_run_ryans_script.sh
@@ -10,6 +10,6 @@ echo "LOGIC -snr < 10 REJECT" > initial.select
 
 echo ""
 echo "Now RUN:"
-echo "tempo2 -gr plk -f initial_flag.par initial_flag.tim -select initial.select"
+echo "tempo2 -gr plk -f initial_flag.par initial_flag.tim -select initial.select -epoch centre -nofit -fit f0 -fit f1 -fit dm -fit dm1"
 echo ""
 echo "write out 'clean.par' and 'clean.tim' when happy"
